d82050b0c18dcb378e3c7ef641d09a69e788d345
ben <benjamin.roy.jones@gmail.com>
2020-12-03 01:10:55 -0600

    use hashMap

--- src/Ticket.hs
+++ src/Ticket.hs
@@ -1,10 +1,11 @@
 module Ticket (ticketSolution) where
 
 import System.IO
+import qualified Data.HashMap.Strict as HM
 
 ticketSolution :: IO String
 ticketSolution = do
-  maybeVals <- sumsTo2020 <$> fileToInts "input.txt"
+  maybeVals <- sumsTo2020 mempty  <$> fileToInts "input.txt"
   return $ case maybeVals of
     Just (a, b) -> show a ++ " times " ++ show b ++ " is " ++ show (a * b)
     Nothing -> "no solution"
@@ -12,14 +13,9 @@ ticketSolution = do
 fileToInts :: FilePath -> IO [Int]
 fileToInts file = map read . lines <$> readFile file
 
-sumsTo2020 :: [Int] -> Maybe (Int, Int)
-sumsTo2020 ints = case ints of
-  (a:b:xs)
-    | Just vals <- checkAgainstEvery a (b:xs) -> Just vals
-    | otherwise -> sumsTo2020 (b:xs)
-  _ -> Nothing
-  where
-    checkAgainstEvery checkInt (x:xs)
-      | checkInt + x == 2020 = Just (checkInt, x)
-      | otherwise = checkAgainstEvery checkInt xs
-    checkAgainstEvery _ _ = Nothing
+sumsTo2020 :: HM.HashMap Int Int -> [Int] -> Maybe (Int, Int)
+sumsTo2020 hm [] = Nothing
+sumsTo2020 hm (x:xs) = case HM.lookup x hm of
+  Just comp -> Just (comp, x)
+  Nothing -> sumsTo2020 (HM.insert (2020 - x) x hm) xs
+
